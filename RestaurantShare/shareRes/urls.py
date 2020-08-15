6f7a58cb201c270536a00e341f6d2b4b48cf3640
claytonhero <clayton@culturehero.net>
2020-08-15 21:07:19 +0900

    0907

--- RestaurantShare/shareRes/urls.py
+++ RestaurantShare/shareRes/urls.py
@@ -3,6 +3,6 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'),
     path('restaurantDetail/',views.restaurantDetail),
-    path('restaurantCreate',views.restaurantCreate),
-    path('categoryCreate',views.categoryCreate)
+    path('restaurantCreate/',views.restaurantCreate),
+    path('categoryCreate/',views.categoryCreate)
 ]
