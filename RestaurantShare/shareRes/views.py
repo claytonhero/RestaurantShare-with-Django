4412b9137c7d2c3f1c23aefe8517702ff6582dc3
claytonhero <clayton@culturehero.net>
2020-08-16 17:53:31 +0900

    why error

--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 # Create your views here.
 
 def index(request):
     # return HttpResponse("index")
-    return render(request,'shareRes/index.html')
+    categories = Category.objects.all()
+    content = {'categories':categories}
+    return render(request,'shareRes/index.html', content)
 
 def restaurantDetail(request):
     # return HttpResponse("restaurantDetail")
@@ -17,3 +21,9 @@ def restaurantCreate(request):
 def categoryCreate(request):
     # return HttpResponse("categoryCreate")
     return render(request,'shareRes/categoryCreate.html')
+
+def Create_category(request):
+    category_name = request.POST['categoryName']
+    new_category = Category(category_name=category_name)
+    new_category.save()
+    return HttpResponseRedirect(reverse('index'))
